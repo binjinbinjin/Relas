c9568ffc0d80af9313e3b616aaa2aafb56e0ccf5
Jin Bin Liu <jinbinjinbin@live.ca>
2018-03-21 19:51:44 -0400

    add the find by User to UserPortfolioRepository

--- src/main/java/relas/java/repository/UserPortfolioRepository.java
+++ src/main/java/relas/java/repository/UserPortfolioRepository.java
@@ -1,10 +1,12 @@
 package relas.java.repository;
 
+import relas.java.domain.User;
 import relas.java.domain.UserPortfolio;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.data.jpa.repository.*;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Spring Data JPA repository for the UserPortfolio entity.
@@ -18,5 +20,7 @@ public interface UserPortfolioRepository extends JpaRepository<UserPortfolio, Lo
 
     @Query("select user_portfolio from UserPortfolio user_portfolio where user_portfolio.userName.login = ?#{principal.username}")
     List<UserPortfolio> findByUserNameIsCurrentUser();
+
+    Optional<UserPortfolio> findOneByUser(User user);
 
 }
