ca4231505f0c4ef35dc3f8c189e6ca59e4bd4458
Jin Bin Liu <jinbinjinbin@live.ca>
2018-03-21 03:16:15 -0400

    add function to get current account id and username

--- src/main/webapp/app/shared/auth/account.service.ts
+++ src/main/webapp/app/shared/auth/account.service.ts
@@ -1,17 +1,35 @@
+import { AccountIDUsername } from './account.service';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { SERVER_API_URL } from '../../app.constants';
 
+/**Account id and user name */
+export interface AccountIDUsername {
+    id: number;
+    username: string;
+}
 @Injectable()
 export class AccountService  {
     constructor(private http: HttpClient) { }
 
-    get(): Observable<HttpResponse<Account>> {
-        return this.http.get<Account>(SERVER_API_URL + 'api/account', {observe : 'response'});
+    get(): Observable<HttpResponse<any>> {
+        return this.http.get<any>(SERVER_API_URL + 'api/account', {observe : 'response'});
     }
 
     save(account: any): Observable<HttpResponse<any>> {
         return this.http.post(SERVER_API_URL + 'api/account', account, {observe: 'response'});
+    }
+
+    /** Get account id and user name */
+    async getAccountKey(): Promise<AccountIDUsername> {
+        let info: AccountIDUsername;
+        await this.get().toPromise().then((response) => {
+            const account = response.body;
+            if (account) {
+               info = {id: response.body.id, username: response.body.login };
+            }
+        });
+        return info;
     }
 }
