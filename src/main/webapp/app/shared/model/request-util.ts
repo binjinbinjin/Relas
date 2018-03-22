c3eb09cc68d0b74c211e7bd927aae97ef2416810
Jin Bin Liu <jinbinjinbin@live.ca>
2018-03-22 19:56:24 -0400

    Angular add a method to generate the HttpParam without paging

--- src/main/webapp/app/shared/model/request-util.ts
+++ src/main/webapp/app/shared/model/request-util.ts
@@ -16,3 +16,19 @@ export const createRequestOption = (req?: any): HttpParams => {
     }
     return options;
 };
+
+/**Return a HttpParams correspond to the input object
+ * eg: {query: '1', gender: MALE} then HttpParams will have value query=1&gender=MALE
+ *
+ * */
+export const creatRequestOptionWithoutPaging = (req?: any): HttpParams => {
+  if(!req)
+      throw Error("req cant be empty in "+ creatRequestOptionWithoutPaging());
+
+  let option: HttpParams = new HttpParams();
+  Object.keys(req).forEach((key)=> {
+      option = option.set(key, req[key]);
+  });
+
+  return option;
+};
