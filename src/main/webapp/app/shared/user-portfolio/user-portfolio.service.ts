626f07e1d6641872997eaa7ebb806c75899752a3
Jin Bin Liu <jinbinjinbin@live.ca>
2018-03-22 20:01:06 -0400

    bug fix

--- src/main/webapp/app/shared/user-portfolio/user-portfolio.service.ts
+++ src/main/webapp/app/shared/user-portfolio/user-portfolio.service.ts
@@ -3,6 +3,7 @@ import { SERVER_API_URL } from '../../app.constants';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { UserPortfolio } from './user-portfolio.model';
+import { Principal } from '..';
 
 export type UserPortfolioServiceConnection = HttpResponse<UserPortfolio>;
 @Injectable()
@@ -10,15 +11,18 @@ export class UserPortfolioService {
 
   private resourceUrl = SERVER_API_URL + 'api/user-portfolios';
   private resourceSearchUrl = SERVER_API_URL + 'api/_search/user-portfolios';
-  constructor(private http: HttpClient) { }
-
-  newPortfolio(portfolio: UserPortfolio): Observable<UserPortfolioServiceConnection> {
-    return this.http.post<UserPortfolio>(this.resourceUrl, portfolio, {observe: 'response'});
-  }
+  constructor(private http: HttpClient, private principal: Principal ) { }
 
   updatePortfolio(portfolio: UserPortfolio): Observable<UserPortfolioServiceConnection>  {
     return this.http.put<UserPortfolio>(this.resourceSearchUrl, portfolio, {observe: 'response'});
   }
 
-  
+  fetchPortfolio(): Observable<UserPortfolioServiceConnection> | null {
+    const login = this.principal.getUserLogin();
+    if (!login) {
+      return null;
+    }
+    return this.http.get<UserPortfolio>(this.resourceSearchUrl + '/login')
+  }
+
 }
