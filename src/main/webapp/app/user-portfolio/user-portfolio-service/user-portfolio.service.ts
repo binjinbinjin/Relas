c0003584a3453dc8caaf342688c06e100b679dfd
Jin Bin Liu <jinbinjinbin@live.ca>
2018-03-23 19:08:04 -0400

    code change of user portfolio service

--- src/main/webapp/app/user-portfolio/user-portfolio-service/user-portfolio.service.ts
+++ src/main/webapp/app/user-portfolio/user-portfolio-service/user-portfolio.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@angular/core';
 import { SERVER_API_URL } from '../../app.constants';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import { UserPortfolio } from './user-portfolio.model';
 import { creatRequestOptionWithoutPaging, createRequestOption } from '../../shared';
 import { Principal } from '../../shared/auth/principal.service';
+import { UserPortfolio } from '../user-portfolio.model';
 
 export type UserPortfolioServiceConnection = HttpResponse<UserPortfolio>;
 @Injectable()
@@ -14,6 +14,7 @@ export class UserPortfolioService {
     private resourceSearchUrl = SERVER_API_URL + 'api/_search/user-portfolios';
     constructor(private http: HttpClient, private principal: Principal ) { }
 
+    /**Update user Porfolio */
     updatePortfolio(portfolio: UserPortfolio): Observable<UserPortfolioServiceConnection>  {
         return this.http.put<UserPortfolio>(this.resourceSearchUrl, portfolio, {observe: 'response'});
     }
@@ -21,28 +22,24 @@ export class UserPortfolioService {
     /**Fetch the portfolio of current user */
     fetchPortfolio(): Observable<UserPortfolioServiceConnection> | null {
         const userLogin = this.principal.getUserLogin();
-        return this.fetchByLogin(userLogin);
+        return this.fetchPortfolioByLogin(userLogin);
     }
 
     /**Fetch portfolio by user login */
     fetchPortfolioByLogin(userLogin: String): Observable<UserPortfolioServiceConnection> | null {
-        return this.fetchByLogin(userLogin);
-    }
-
-    /**Fetch the portfolios by gender */
-    getPortfolioByGender(req): Observable<HttpResponse<UserPortfolio>> {
-        if (!req)
-            throw Error('cant be null');
-        const option = createRequestOption(req);
-
-        return this.http.get<UserPortfolio>(this.resourceSearchUrl + '/gender', {params: option, observe: 'response'});
-    }
-
-    private fetchByLogin(userLogin: String): Observable<UserPortfolioServiceConnection> | null {
         if (!userLogin)
             return null;
         const option = creatRequestOptionWithoutPaging({ login: userLogin });
         return this.http.get<UserPortfolio>(this.resourceSearchUrl + '/login', { params: option, observe: 'response' });
+    }
+
+    /**Fetch the portfolios by gender */
+    getPortfolioByGender(req): Observable<HttpResponse<UserPortfolio[]>> {
+        if (!req)
+            throw Error('cant be null');
+        const option = createRequestOption(req);
+
+        return this.http.get<UserPortfolio[]>(this.resourceSearchUrl + '/gender', {params: option, observe: 'response'});
     }
 
 }
