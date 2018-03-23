3509baa31f0f6f4071a9ecc4a21d991ed10e756e
Jin Bin Liu <jinbinjinbin@live.ca>
2018-03-23 19:09:06 -0400

    Complete the search bar for options of search by gender and user login

--- src/main/webapp/app/user-portfolio/portfolio-search/search-bar/search-bar.component.ts
+++ src/main/webapp/app/user-portfolio/portfolio-search/search-bar/search-bar.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UserPortfolioService } from '../../user-portfolio-service/user-portfolio.service';
+import { UserPortfolio, gender } from '../../user-portfolio.model';
+import { PortfolioSearchOption } from '../search-util';
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styles: []
 })
-export class SearchBarComponent implements OnInit {
 
-  constructor() { }
+/***Search bar for user portfolio search */
+export class SearchBarComponent implements OnInit {
+  @Output('searchResult') result: EventEmitter<PortfolioSearchOption>;
+  userGender;
+  constructor(private userPortfolioService: UserPortfolioService) {
+    this.result = new EventEmitter();
+    this.userGender = gender;
+   }
 
   ngOnInit() {
+  }
+
+  /**emit the seach request */
+  onSubmit(form: NgForm) {
+    const value = form.value;
+    if (value.searchOption === 'gender') {
+      this.result.emit({useLogin: false, value: value.gender});
+      return;
+    }
+    this.result.emit({useLogin: true, value: value.search});
   }
 
 }
