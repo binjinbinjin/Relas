74f4fefb545ad619f9c737b8776051099b0b7b33
Jin Bin Liu <jinbinjinbin@live.ca>
2018-03-23 19:13:50 -0400

    Complete search and display for search by gender and user login for userPortfolio

--- src/main/webapp/app/user-portfolio/portfolio-search/portfolio-search/portfolio-search.component.ts
+++ src/main/webapp/app/user-portfolio/portfolio-search/portfolio-search/portfolio-search.component.ts
@@ -1,15 +1,83 @@
+import { gender } from '../../user-portfolio.model';
 import { Component, OnInit } from '@angular/core';
-
+import { PortfolioSearchOption } from '../search-util';
+import { UserPortfolioService } from '../../user-portfolio-service/user-portfolio.service';
+import { UserPortfolio } from '../../user-portfolio.model';
+import { ITEMS_PER_PAGE } from '../../../shared';
 @Component({
   selector: 'app-portfolio-search',
   templateUrl: './portfolio-search.component.html',
   styles: []
 })
+
+/***Perform search and display the result of search for UserPortfolio */
 export class PortfolioSearchComponent implements OnInit {
 
-  constructor() { }
+  /**page number of search result */
+  pageNumber: number;
+
+  /**Search result */
+  result: UserPortfolio[];
+
+  /**record of the value of last gender search */
+  reqGenderHistory: gender;
+  constructor(private userPortfolioService: UserPortfolioService) {
+    this.result = [];
+    this.pageNumber = -1;
+   }
 
   ngOnInit() {
   }
 
+  /***Search the user portfolio by user request */
+  search(req: PortfolioSearchOption) {
+    if (!req.useLogin) {
+      this.reqGenderHistory = (req.value as gender);
+      this.pageNumber = 0;
+      this.getPortfoliosByGender((req.value as gender));
+      return;
+    }
+
+    this.pageNumber = -1;
+    this.getPortfoliosByLogin((req.value as string));
+  }
+
+  /**Changes the search result of gender by page number
+   * (To enable the function, reqGenderHistory is required, that means user must performed search by
+   * gender at least once, then user can request a search page change)
+   */
+  pageGenderChange(page: number) {
+    if (!this.reqGenderHistory)
+      return;
+    this.pageNumber = page;
+    this.getPortfoliosByGender(this.reqGenderHistory);
+  }
+
+  /**Get the users porfolios by gender */
+  private getPortfoliosByGender(searchGender: gender) {
+    this.userPortfolioService.getPortfolioByGender({ gender: searchGender, ...this.page }).toPromise().then((response) => {
+      this.result = response.body;
+      if (this.result.length === 0) {
+        this.pageGenderChange(this.pageNumber - 1);
+
+      }
+    });
+  }
+
+  /**Get user portfolio by user login */
+  private getPortfoliosByLogin(login: string) {
+    this.userPortfolioService.fetchPortfolioByLogin(login).toPromise().then((response) => {
+      this.result = [];
+      if (response.ok) {
+        this.result.push(response.body);
+      }
+    }).catch((error) => {
+      this.result = [];
+    });
+  }
+
+  /**Generate pagination info for search */
+  get page() {
+    return { page: this.pageNumber, size: ITEMS_PER_PAGE, sort: ['id', 'asc'] };
+  }
 }
