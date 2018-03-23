1f4dd05559da5e0076501f3694d1ebbe8581f3d6
Jin Bin Liu <jinbinjinbin@live.ca>
2018-03-23 19:07:15 -0400

    file location change

--- src/main/webapp/app/user-portfolio/user-portfolio.module.ts
+++ src/main/webapp/app/user-portfolio/user-portfolio.module.ts
@@ -1,14 +1,22 @@
-import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PortfolioSearchComponent } from './portfolio-search/portfolio-search/portfolio-search.component';
 import { PortfolioChangeComponent } from './portfolio-change/portfolio-change.component';
 import { SearchResultDisplayComponent } from './portfolio-search/search-result-display/search-result-display.component';
 import { SearchBarComponent } from './portfolio-search/search-bar/search-bar.component';
+import { UserPortfolioService } from './user-portfolio-service/user-portfolio.service';
+import { RelasSharedModule } from '../shared';
 
 @NgModule({
+  exports: [PortfolioSearchComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    RelasSharedModule,
   ],
-  declarations: [PortfolioSearchComponent, PortfolioChangeComponent, SearchResultDisplayComponent, SearchBarComponent]
+  providers: [UserPortfolioService],
+  declarations: [PortfolioSearchComponent, PortfolioChangeComponent, SearchResultDisplayComponent, SearchBarComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class UserPortfolioModule { }
