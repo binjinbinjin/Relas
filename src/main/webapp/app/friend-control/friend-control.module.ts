c9944e08b05a71cbf3484234f5967c72b297c0af
Jin Bin Liu <jinbinjinbin@live.ca>
2018-03-25 03:02:55 -0400

    add a module to deal with all users relationship

--- src/main/webapp/app/friend-control/friend-control.module.ts
+++ src/main/webapp/app/friend-control/friend-control.module.ts
@@ -1,10 +1,23 @@
+import { FormsModule } from '@angular/forms';
+import { RelasSharedCommonModule } from './../shared/shared-common.module';
+import { Principal } from 'src/main/webapp/app/shared/auth/principal.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AddFriendComponent } from './add-friend/add-friend.component';
+import { UserPortfolioModule } from '../user-portfolio/user-portfolio.module';
+import { FriendshipService } from './friend-control-services/friendship.service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    UserPortfolioModule,
+    FormsModule,
+    RelasSharedCommonModule,
+    NgbModule
   ],
-  declarations: []
+  exports: [AddFriendComponent],
+  providers: [FriendshipService],
+  declarations: [AddFriendComponent]
 })
 export class FriendControlModule { }
