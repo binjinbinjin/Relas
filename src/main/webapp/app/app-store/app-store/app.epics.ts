365b98d24ac8fdc3cc74eef28a933d18f5c6bcfb
Jin Bin Liu <jinbinjinbin@live.ca>
2018-03-28 18:07:42 -0400

    Add root level Epic

--- src/main/webapp/app/app-store/app-store/app.epics.ts
+++ src/main/webapp/app/app-store/app-store/app.epics.ts
@@ -1,11 +1,9 @@
-import { Injectable } from '@angular/core';
+import { FriendshipService } from '../friendship.service';
+import { getFriendRequest } from '../friend-control/friend-control.epics';
 
-@Injectable()
-export class RootEpics {
-    constructor() { }
-
-    public createEpics() {
+/**App(root) level Epics, all the epics will add here */
+export function createEpics(friendshipService: FriendshipService) {
         return [
+            getFriendRequest(friendshipService) // Epics for friend-control
         ];
-    }
 }
