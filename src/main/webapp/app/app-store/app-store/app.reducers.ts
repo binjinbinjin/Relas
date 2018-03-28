30e6fec6f91e997c7079db478131551c95d8b104
Jin Bin Liu <jinbinjinbin@live.ca>
2018-03-28 18:09:41 -0400

    Add root level reducer

--- src/main/webapp/app/app-store/app-store/app.reducers.ts
+++ src/main/webapp/app/app-store/app-store/app.reducers.ts
@@ -1,9 +1,15 @@
-import { combineReducers } from 'redux';
 import { composeReducers, defaultFormReducer } from '@angular-redux/form';
 import { routerReducer } from '@angular-redux/router';
+import { combineReducers } from 'redux';
 
-export const rootReducer = composeReducers(
+import { FriendshipService } from '../friendship.service';
+import { receivedRequestReducer, sendRequestReducer } from '../friend-control/friend-control.reducers';
+
+/**App(root) level reducer, all the epics will add here */
+export const rootReducer = (friendshipService: FriendshipService) => composeReducers(
     defaultFormReducer(),
     combineReducers({
+        addFriend: sendRequestReducer(friendshipService),
+        receivedRequest: receivedRequestReducer(),
         router: routerReducer,
     }));
