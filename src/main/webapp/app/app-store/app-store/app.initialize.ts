5e8980575394b68fec3c9f58e3fafdfcce65783e
Jin Bin Liu <jinbinjinbin@live.ca>
2018-03-28 18:24:29 -0400

    add comment

--- src/main/webapp/app/app-store/app-store/app.initialize.ts
+++ src/main/webapp/app/app-store/app-store/app.initialize.ts
@@ -5,12 +5,16 @@ import { createSendRequstAction, FriendControlActionsList } from '../friend-cont
 import { FriendshipService } from './../friendship.service';
 import { StoreDataStatus } from './app.store.model';
 
+/**This class is use to initilize all the stuff that have to be initialize for the store */
 export class AppStoreInitializer {
 
     static initialize(friendshipService: FriendshipService): void {
         AppStoreInitializer.friendControlReceiveRequest();
     }
 
+    /**
+     * dispatch an action to start to get the request from
+     */
     @dispatch()
     private static friendControlReceiveRequest() {
         return createSendRequstAction(FriendControlActionsList.RECEIVED_REQUEST, {dataStatus: StoreDataStatus.INITIALIZE});
