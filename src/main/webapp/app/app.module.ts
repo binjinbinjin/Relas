1fb92e0375887ad8cbd470fd42cea79bc0a9b753
Jin Bin Liu <jinbinjinbin@live.ca>
2018-03-28 18:26:40 -0400

    import redux at app module

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -1,35 +1,36 @@
 import './vendor.ts';
 
-import { NgModule, Injector } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgReduxRouterModule } from '@angular-redux/router';
+import { NgReduxModule } from '@angular-redux/store';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { Ng2Webstorage, LocalStorageService, SessionStorageService  } from 'ngx-webstorage';
+import { Injector, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
+import { LocalStorageService, Ng2Webstorage, SessionStorageService } from 'ngx-webstorage';
 
-import { AuthInterceptor } from './blocks/interceptor/auth.interceptor';
+import { RelasAccountModule } from './account/account.module';
+import { RelasAdminModule } from './admin/admin.module';
+import { RelasAppRoutingModule } from './app-routing.module';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
 import { AuthExpiredInterceptor } from './blocks/interceptor/auth-expired.interceptor';
+import { AuthInterceptor } from './blocks/interceptor/auth.interceptor';
 import { ErrorHandlerInterceptor } from './blocks/interceptor/errorhandler.interceptor';
 import { NotificationInterceptor } from './blocks/interceptor/notification.interceptor';
-import { RelasSharedModule, UserRouteAccessService } from './shared';
-import { RelasAppRoutingModule} from './app-routing.module';
-import { RelasHomeModule } from './home/home.module';
-import { RelasAdminModule } from './admin/admin.module';
-import { RelasAccountModule } from './account/account.module';
 import { RelasEntityModule } from './entities/entity.module';
-import { PaginationConfig } from './blocks/config/uib-pagination.config';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-// jhipster-needle-angular-add-module-import JHipster will add new module here
+import { RelasHomeModule } from './home/home.module';
 import {
+    ActiveMenuDirective,
+    ErrorComponent,
+    FooterComponent,
     JhiMainComponent,
     NavbarComponent,
-    FooterComponent,
-    ProfileService,
     PageRibbonComponent,
-    ActiveMenuDirective,
-    ErrorComponent
+    ProfileService,
 } from './layouts';
+import { RelasSharedModule, UserRouteAccessService } from './shared';
 
+// jhipster-needle-angular-add-module-import JHipster will add new module here
 @NgModule({
     imports: [
         BrowserModule,
@@ -41,6 +42,8 @@ import {
         RelasAccountModule,
         RelasEntityModule,
         NgbModule.forRoot(),
+        NgReduxModule,
+        NgReduxRouterModule.forRoot(),
         // jhipster-needle-angular-add-module JHipster will add new module here
     ],
     declarations: [
