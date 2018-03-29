6c51a498e3ba449c6dd228eb1eddf649d91afbbb
Jin Bin Liu <jinbinjinbin@live.ca>
2018-03-29 17:30:50 -0400

    filter out add friends request from a bunch of requests

--- src/main/webapp/app/friend-control/show-friend-request/show-friend-request.component.ts
+++ src/main/webapp/app/friend-control/show-friend-request/show-friend-request.component.ts
@@ -1,10 +1,18 @@
-import { select } from '@angular-redux/store';
+import { select, select$ } from '@angular-redux/store';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
 import { FriendshipRequest } from '../friend-control-services/friend-request-model';
 import { RECEIVED_REQUEST } from './../../app-store/friend-control/friend-control.data';
+import { StoreDataInter } from '../../app-store/app-store/app.store.model';
 
+export const getRequst = (payloads$: Observable<{}>) => {
+  return payloads$.map((value) => {
+    const state = (value as StoreDataInter<FriendshipRequest>);
+    const payloads = state.payloads;
+    return payloads.filter((item) => item.introduceToId === item.introduceById);
+  });
+};
 @Component({
   selector: 'app-show-friend-request',
   templateUrl: './show-friend-request.component.html',
@@ -13,14 +21,14 @@ import { RECEIVED_REQUEST } from './../../app-store/friend-control/friend-contro
 export class ShowFriendRequestComponent implements OnInit {
 
   /**Get the requests from store */
-  @select([RECEIVED_REQUEST, 'payloads'])
-  readonly requsts: Observable<FriendshipRequest[]>;
+  @select$([RECEIVED_REQUEST], getRequst)
+  readonly payloads: Observable<FriendshipRequest[]>;
   req: any;
   constructor() { }
 
   ngOnInit() {
     this.req = { };
-    this.requsts.subscribe((response) => {
+    this.payloads.subscribe((response) => {
       this.req = response;
     });
   }
