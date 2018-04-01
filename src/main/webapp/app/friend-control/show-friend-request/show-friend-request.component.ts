a84ee8721e83635790ea80002cf81ef96eb05566
Jin Bin Liu <jinbinjinbin@live.ca>
2018-04-01 05:18:40 -0400

    ability to accept the request

--- src/main/webapp/app/friend-control/show-friend-request/show-friend-request.component.ts
+++ src/main/webapp/app/friend-control/show-friend-request/show-friend-request.component.ts
@@ -1,10 +1,14 @@
-import { select, select$ } from '@angular-redux/store';
+import { select$ } from '@angular-redux/store';
 import { Component, OnInit } from '@angular/core';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { Observable } from 'rxjs/Observable';
 
-import { FriendshipRequest } from '../friend-control-services/friend-request-model';
-import { RECEIVED_REQUEST } from './../../app-store/friend-control/friend-control.data';
 import { StoreDataInter } from '../../app-store/app-store/app.store.model';
+import { FriendshipControlService } from '../../app-store/service/friendshipControl.service';
+import { FriendshipRequest } from '../friend-control-model/friend-request-model';
+import { RECEIVED_REQUEST } from './../../app-store/friend-control/friend-control.data';
+import { PortfolioSearchOption } from '../../user-portfolio/portfolio-search/search-util';
+import { FriendControlActionModel, FriendControlActionEnum } from '../friend-control-model/friend-control-action.model';
 
 export const getRequst = (payloads$: Observable<{}>) => {
   return payloads$.map((value) => {
@@ -23,8 +27,17 @@ export class ShowFriendRequestComponent implements OnInit {
   /**Get the requests from store */
   @select$([RECEIVED_REQUEST], getRequst)
   readonly payloads: Observable<FriendshipRequest[]>;
+
+  /**An array of user requests */
   req: any;
-  constructor() { }
+
+  /**Record of a PortfolioSearchOption of a request sender, and request id*/
+  requestUser;
+
+  /**Reference of open modal */
+  modalRef: NgbModalRef;
+
+  constructor(private modalService: NgbModal, private friendshipControlService: FriendshipControlService) { }
 
   ngOnInit() {
     this.req = { };
@@ -33,4 +46,25 @@ export class ShowFriendRequestComponent implements OnInit {
     });
   }
 
+  /**Show the userPorfolio of a request sender by login */
+  showPortfolio(login: string, id: number, content) {
+    this.requestUser = {
+      search: ({useLogin: true, value: login} as PortfolioSearchOption),
+      requestId: id
+    };
+    this.modalRef = this.modalService.open(content);
+  }
+
+  closeModal(add) {
+    this.modalRef.close();
+    if (!add) {
+        this.requestUser = null;
+        return;
+    }
+    this.addFriend(add.search.value);
+  }
+
+  addFriend(targetLogin: string) {
+    this.friendshipControlService.addFriend(new FriendControlActionModel(FriendControlActionEnum.ADD_FRIEND, targetLogin));
+  }
 }
