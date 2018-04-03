d0041b56fc9b0f92b6d2a7aaaaa83fa7869b81cd
Jin Bin Liu <jinbinjinbin@live.ca>
2018-04-03 06:42:15 -0400

    Nothing change

--- src/main/webapp/app/user-portfolio/user-portfolio.module.ts
+++ src/main/webapp/app/user-portfolio/user-portfolio.module.ts
@@ -5,14 +5,13 @@ import { PortfolioSearchComponent } from './portfolio-search/portfolio-search/po
 import { PortfolioChangeComponent } from './portfolio-change/portfolio-change.component';
 import { SearchResultDisplayComponent } from './portfolio-search/search-result-display/search-result-display.component';
 import { SearchBarComponent } from './portfolio-search/search-bar/search-bar.component';
-import { UserPortfolioService } from './user-portfolio-service/user-portfolio.service';
 import { RelasSharedModule } from '../shared';
 import { PortfolioChangeFormComponent } from './portfolio-change-form/portfolio-change-form.component';
 import { JhiDataUtils } from 'ng-jhipster';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DirectivesModule } from '../directives/directives.module';
+import { UserPortfolioService } from './user-portfolio-service/user-portfolio.service';
 @NgModule({
-  exports: [PortfolioSearchComponent, PortfolioChangeComponent],
   imports: [
     CommonModule,
     FormsModule,
@@ -20,8 +19,9 @@ import { DirectivesModule } from '../directives/directives.module';
     NgbModule,
     DirectivesModule
   ],
-  providers: [UserPortfolioService, JhiDataUtils],
   declarations: [PortfolioSearchComponent, PortfolioChangeComponent, SearchResultDisplayComponent, SearchBarComponent, PortfolioChangeFormComponent],
+  providers: [UserPortfolioService, JhiDataUtils],
+  exports: [PortfolioSearchComponent, PortfolioChangeComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class UserPortfolioModule { }
