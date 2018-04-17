669bc13657cc718a2c3e8a2b937398db33aa8137
Jin Bin Liu <jinbinjinbin@live.ca>
2018-04-16 21:11:44 -0400

    Changed(Add more action) the friend control actions in app store

--- src/main/webapp/app/friend-control/friend-control-model/friend-control-action.model.ts
+++ src/main/webapp/app/friend-control/friend-control-model/friend-control-action.model.ts
@@ -1,18 +1,26 @@
 
+/**Actions for FriendControl in app store */
 export const  enum FriendControlActionEnum {
     ADD_FRIEND = 'ADD_FRIEND',
     DELETE_FRIEND = 'DELETE_FRIEND',
     NEW_FRIEND = 'NEW_FRIEND',
     GET_LIST = 'GET_LIST',
     NEW_FRIEND_LIST = 'NEW_FRIEND_LIST',
+    ADD_PORFOLIO_FOR_FRIEND = 'ADD_PORFOLIO_FOR_FRIEND',
 }
 
+/**Model for modify the friend in frien list (Model for app store)*/
 export class FriendControlActionModel {
     userLogin?: string;
     targetLogin: string;
     action: FriendControlActionEnum;
     reason?: string;
-    constructor(action: FriendControlActionEnum, targetLogin: string, reason?: string, userLogin?: string) {
+    constructor(
+        action: FriendControlActionEnum,
+        targetLogin: string,
+        reason?: string,
+        userLogin?: string) {
+
         this.action = action;
         this.targetLogin = targetLogin;
         if (userLogin)
