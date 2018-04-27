ff9ad33364ab4ea1becd8ea4cad47d7918dfcec5
Jin Bin Liu <jinbinjinbin@live.ca>
2018-04-27 02:31:01 -0400

    auto reload the page after user login; reason of reload the page is to fresh the app store

--- src/main/webapp/app/shared/login/login.component.ts
+++ src/main/webapp/app/shared/login/login.component.ts
@@ -63,6 +63,7 @@ export class JhiLoginModalComponent implements AfterViewInit {
 
             // // previousState was set in the authExpiredInterceptor before being redirected to login modal.
             // // since login is succesful, go to stored previousState and clear previousState
+            window.location.reload();
             const redirect = this.stateStorageService.getUrl();
             if (redirect) {
                 this.stateStorageService.storeUrl(null);
