f287817edd318698a65eab478d93d1c6c6055f3e
Jin Bin Liu <jinbinjinbin@live.ca>
2018-05-15 00:18:45 -0400

    get query info

--- src/main/webapp/app/feed/display-feed/feeds/feeds.component.ts
+++ src/main/webapp/app/feed/display-feed/feeds/feeds.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'jhi-feeds',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public route: ActivatedRoute, public router: Router) { }
 
   ngOnInit() {
+    this.route.queryParams.subscribe((res) => {
+      console.log('query: ', res.who);
+    });
   }
 
 }
