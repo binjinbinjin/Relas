b56065cc0c06ff44bf51010928e247989cb8343f
Jin Bin Liu <jinbinjinbin@live.ca>
2018-05-15 00:20:33 -0400

    Bug fix, and add router module

--- src/main/webapp/app/feed/feed.module.ts
+++ src/main/webapp/app/feed/feed.module.ts
@@ -1,14 +1,27 @@
-import { NgModule } from '@angular/core';
+import { feedMainRoute } from './feed-routes';
 import { CommonModule } from '@angular/common';
-import { PostComponent } from './post/post/post.component';
-import { FeedsComponent } from './display-feed/feeds/feeds.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { JhiDataUtils } from 'ng-jhipster';
+
+import { Principal } from '../shared/auth/principal.service';
 import { EachFeedComponent } from './display-feed/each-feed/each-feed.component';
 import { FeedContentComponent } from './display-feed/feed-content/feed-content.component';
+import { FeedsComponent } from './display-feed/feeds/feeds.component';
+import { PostComponent } from './post/post/post.component';
+import { FeedService } from './service/feed.service';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forChild(feedMainRoute),
   ],
-  declarations: [PostComponent, FeedsComponent, EachFeedComponent, FeedContentComponent]
+  declarations: [PostComponent, FeedsComponent, EachFeedComponent, FeedContentComponent],
+  exports: [PostComponent, FeedsComponent, EachFeedComponent, FeedContentComponent],
+  providers: [JhiDataUtils, Principal, FeedService],
 })
 export class FeedModule { }
