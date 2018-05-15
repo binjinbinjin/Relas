cfe9c1d5903a6cef99e2d5df9997f8ae7f3bc3da
Jin Bin Liu <jinbinjinbin@live.ca>
2018-05-15 00:18:22 -0400

    Complete component for post new feed

--- src/main/webapp/app/feed/post/post/post.component.ts
+++ src/main/webapp/app/feed/post/post/post.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { JhiDataUtils } from 'ng-jhipster';
+import { ChatMessageImageDisplayModel } from '../../../chat/model/chat-room.model';
+import { FeedService } from '../../service/feed.service';
+import { FeedModel } from '../../model/feed.model';
 
+/**Component for post new feed */
 @Component({
-  selector: 'jhi-post',
+  selector: 'app-post',
   templateUrl: './post.component.html',
-  styles: []
+  styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
 
-  constructor() { }
+  @Input('userId') id;
+  /**Variable for wrapping the image data*/
+  image: ChatMessageImageDisplayModel;
+
+  constructor(private dataUtils: JhiDataUtils, private feedService: FeedService) {
+    this.image = new ChatMessageImageDisplayModel(null, null);
+  }
+  /**Get image file from user */
+  setFileData(event, entity, field, isImage) {
+    this.dataUtils.setFileData(event, entity, field, isImage);
+  }
 
   ngOnInit() {
   }
 
+  /**Post new feed */
+  post(text, imageData, imageType) {
+    const feed = new FeedModel();
+    feed.userIDId = this.id;
+    feed.time = new Date();
+    feed.message = text;
+    feed.accessoryContentType = imageType;
+    feed.accessory = imageData;
+    this.feedService.create(feed).subscribe((res) => {
+
+    });
+
+  }
 }
